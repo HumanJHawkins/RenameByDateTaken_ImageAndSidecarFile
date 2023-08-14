52192a1c5616dff57dd95f90b2016056a5909baa
humanjhawkins <18685023+HumanJHawkins@users.noreply.github.com>
2023-08-14 00:30:59 -0700

    Slightly better error prevention.

--- RenameImageAndDOP_creator.bat
+++ RenameImageAndDOP_creator.bat
@@ -1,9 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Check if the user has provided a directory
+if "%~1"=="" (
+    echo Error: Please provide a directory.
+    exit /b
+)
+
 REM Set the root directory
-set rootDirectory=%1
-if not defined rootDirectory set rootDirectory=.
+set rootDirectory=%~1
+
+REM Convert all file extensions to lowercase using PowerShell
+echo Converting all file extensions to lowercase...
+for /R "%rootDirectory%" %%F in (*.*) do (
+    powershell -command "Get-Item \"%%F\" | Rename-Item -NewName { $_.BaseName + $_.Extension.ToLower() }"
+)
+for /R "%rootDirectory%" %%F in (*.*.dop) do (
+    powershell -command "$item = Get-Item \"%%F\"; $basename = $item.BaseName; $extension = $item.Extension; $dotIndex = $basename.LastIndexOf('.'); if ($dotIndex -ge 0) { $basename = $basename.Substring(0, $dotIndex) + $basename.Substring($dotIndex).ToLower() }; Rename-Item -Path $item.FullName -NewName ( $basename + $extension )"
+)
+echo Done converting extensions.
 
 REM Create a new text file with the current date/time as its name
 for /f "delims=" %%a in ('wmic OS Get localdatetime  ^| find "."') do set currentDateTime=%%a
